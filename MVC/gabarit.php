5834f4a86ef2750826cdf0268149f14530098a52
ALHELO Marwan <Marwan.ALHELO@cpnv.ch>
2019-12-19 12:18:40 +0100

    update

--- MVC/gabarit.php
+++ MVC/gabarit.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Titre</title>
+    <title><?= $title ?></title>
     <script src="/node_modules/jquery/dist/jquery.js"></script>
     <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.css">
     <script src="/node_modules/bootstrap/dist/js/bootstrap.js"></script>
@@ -13,7 +13,7 @@
     <img src="/images/CPNVinfo.png" width="100px">
 </header>
 <div class="container">
-Contenu
+    <?= $content ?>
 </div>
 <footer class="text-center">
     <img src="/images/fb.png" width="20px" height="20px">
--- MVC/index.php
+++ MVC/index.php
@@ -1,3 +1,30 @@
 <?php
+if(isset($_GET['action'])){
+    $action = $_GET['action'];
+}
+else{
+    $action= 'home';
+}
+
+switch ($action)
+{
+    case 'movies';
+        $title = 'Films';
+       /* $content= 'Liste des films';*/
+    require_once 'movies.php';
+        break;
+
+    case 'concerts';
+        $title = 'conserts';
+        /*$content= 'Liste des Conserts';*/
+    require_once 'concerts.php';
+        break;
+
+    default ;
+        $title = 'HOme';
+        require_once 'home.php';
+       /* $content= 'Accueil';*/
+        break;
+}
 require_once 'gabarit.php';
 ?>
